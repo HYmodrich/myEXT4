db52d76c35514d7d2bd9af0953acf0ad8a956fbd
koo <rnrudgh@gmail.com>
2019-12-12 14:55:32 +0900

    complete: system call setting commit delay \n Change the number of system calls in the systemcall table 436-> 437 ,Modify system call to use SYSCALL_DEFINE1

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -854,7 +854,7 @@ __SYSCALL(__NR_clone3, sys_clone3)
 __SYSCALL(__NR_changetime, sys_changetime)
 
 #undef __NR_syscalls
-#define __NR_syscalls 436
+#define __NR_syscalls 437
 
 /*
  * 32 bit systems traditionally used different
