9c8f13859735d0385c9e420b947d9c6d78177ace
HYmodrich <lwj0337@naver.com>
2020-01-13 21:20:48 +0900

    ext4-syscall

--- fs/jbd2/commit.c
+++ fs/jbd2/commit.c
@@ -1169,5 +1169,5 @@ restart_loop:
 	/*lwj*/
         ktime_get_real_ts64(&commit_transaction->tx_flush_end_time);
 
-        printk("\{ \"dev\":%d, \"handle\":%d, \"tid\":%d, \"blocks\":%d, \"pid\":%d, \"lat_commit\":%ld, \} \n", journal->j_dev->bd_dev, commit_transaction->t_handle_count.counter, commit_transaction->t_tid, commit_transaction->lwj_t_nr_buffers, current->pid, (commit_transaction->tx_flush_end_time.tv_sec*1000000+commit_transaction->tx_flush_end_time.tv_nsec/1000) - (commit_transaction->tx_commit_start_time.tv_sec*1000000+commit_transaction->tx_commit_start_time.tv_nsec/1000));
+        printk("\{ \"dev\":%d, \"handle\":%d, \"tid\":%d, \"blocks\":%d, \"pid\":%d, \"lat_commit\":%ld \} \n", journal->j_dev->bd_dev, commit_transaction->t_handle_count.counter, commit_transaction->t_tid, commit_transaction->lwj_t_nr_buffers, current->pid, (commit_transaction->tx_flush_end_time.tv_sec*1000000+commit_transaction->tx_flush_end_time.tv_nsec/1000) - (commit_transaction->tx_commit_start_time.tv_sec*1000000+commit_transaction->tx_commit_start_time.tv_nsec/1000));
 }
